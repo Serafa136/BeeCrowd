0a1613a75b8e4b64e1fd6d033a5d851dbdd61096
Lucas Gabriel <lucasribeiro1293@gmail.com>
2025-02-05 15:43:18 -0300

    Bee
    NewBee

---
+++ BeeCrowd0002.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class BeeCrowd0002 {
+    public static void main (String[]args){
+        Scanner entrada = new Scanner (System.in);
+        int n1 = entrada.nextInt();
+        int n2 = entrada.nextInt();
+        System.out.println("X = " + (n1 + n2));
+        entrada.close();
+
+    }
+}
